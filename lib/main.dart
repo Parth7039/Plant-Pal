ae719b239d3102c318ab203fef2a0507d63fa5e0
Parth Bhamare <p.k.bhamare07@gmail.com>
2024-03-18 19:29:14 +0530

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:medigard/firebase_options.dart';
+import 'package:hive/hive.dart'; // Import Hive package
 import 'Auth_pages/auth_page.dart';
 
-void main()async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Initialize Hive
+  await Hive.initFlutter();
+
+  // Open Hive box
+  await Hive.openBox('myBox');
+
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
