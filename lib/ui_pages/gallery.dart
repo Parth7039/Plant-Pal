a233cff1eeebff881681abd9d0e4e2823f81720d
Parth Bhamare <p.k.bhamare07@gmail.com>
2024-03-18 18:48:42 +0530

    hello

--- lib/ui_pages/gallery.dart
+++ lib/ui_pages/gallery.dart
@@ -1,7 +1,8 @@
-import 'dart:typed_data';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:medigard/ui_pages/images.dart';
 import 'package:path/path.dart' as path;
 
 import 'homepage.dart';
@@ -14,14 +15,14 @@ class GalleryPage extends StatefulWidget {
 }
 
 class _GalleryPageState extends State<GalleryPage> {
-  List<PlatformFile>? files = [];
+  List<File>? files = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.black,
-        onPressed: pickFiless,
+        onPressed: pickFiles,
         child: Icon(Icons.add, color: Colors.white),
       ),
       appBar: AppBar(
@@ -36,49 +37,82 @@ class _GalleryPageState extends State<GalleryPage> {
           icon: Icon(Icons.arrow_back_ios_new_sharp),
         ),
       ),
-      body: GridView.builder(
-        itemCount: files!.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 4.0,
-          mainAxisSpacing: 4.0,
-        ),
-        itemBuilder: (context, index) {
-          return GridTile(
-            child: Image.network(files![index].path!, fit: BoxFit.cover,),
-          );
-        },
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              itemCount: files!.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 4.0,
+                mainAxisSpacing: 4.0,
+              ),
+              itemBuilder: (context, index) {
+                return GridTile(
+                  child: Image.file(files![index], fit: BoxFit.cover),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                ),
+                onPressed: () {
+                  uploadFiles(files!);
+                  clearGridView(); // Call function to clear GridView
+                },
+                child: Text('Save', style: TextStyle(color: Colors.white)),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                ),
+                onPressed: () {
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => DisplayImagePage()) ); // Call function to clear GridView
+                },
+                child: Text('View', style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+          SizedBox(height: 10)
+        ],
       ),
     );
   }
 
-  void pickFiless() async {
+  void pickFiles() async {
     try {
-      FilePickerResult? result = await FilePicker.platform.pickFiles(
-        type: FileType.image,
-        allowMultiple: true,
-      );
+      List<File> pickedFiles = [];
+      final result = await FilePicker.platform.pickFiles(type: FileType.image, allowMultiple: true);
+
       if (result != null) {
-        List<PlatformFile> pickedFiles = result.files;
+        for (var file in result.files) {
+          final filePath = file.path;
+          final fileData = File(filePath!);
+          pickedFiles.add(fileData);
+        }
+
         setState(() {
           files = pickedFiles;
         });
-        uploadFiles(files!);
       }
     } catch (e) {
       print('Error picking files: $e');
     }
   }
 
-  Future<void> uploadFiles(List<PlatformFile> files) async {
+  Future<void> uploadFiles(List<File> files) async {
     try {
       for (var file in files) {
-        final fileName = path.basename(file.name!);
+        final fileName = path.basename(file.path);
         final Reference storageReference =
         FirebaseStorage.instance.ref().child('images/$fileName');
-        final fileData = Uint8List.fromList(file.bytes!);
-
-        final UploadTask uploadTask = storageReference.putData(fileData);
+        final UploadTask uploadTask = storageReference.putFile(file);
 
         final TaskSnapshot taskSnapshot = await uploadTask;
 
@@ -88,5 +122,11 @@ class _GalleryPageState extends State<GalleryPage> {
     } catch (e) {
       print('Error uploading files: $e');
     }
+  }
+
+  void clearGridView() {
+    setState(() {
+      files = [];
+    });
   }
 }
---
+++ lib/ui_pages/images.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:medigard/ui_pages/gallery.dart';
+
+import 'homepage.dart';
+
+class DisplayImagePage extends StatefulWidget {
+  const DisplayImagePage({Key? key}) : super(key: key);
+
+  @override
+  _DisplayImagePageState createState() => _DisplayImagePageState();
+}
+
+class _DisplayImagePageState extends State<DisplayImagePage> {
+  late List<String> imageUrls = []; // Initialize imageUrls with an empty list
+  bool isLoading = true; // Track loading state
+
+  @override
+  void initState() {
+    super.initState();
+    fetchImageUrls();
+  }
+
+  Future<void> fetchImageUrls() async {
+    try {
+      final Reference storageReference = FirebaseStorage.instance.ref().child('images');
+      final ListResult result = await storageReference.listAll();
+      List<String> urls = [];
+      for (final ref in result.items) {
+        final url = await ref.getDownloadURL();
+        urls.add(url);
+      }
+      setState(() {
+        imageUrls = urls;
+        isLoading = false; // Set loading state to false when images are loaded
+      });
+    } catch (e) {
+      print('Error fetching image URLs: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => GalleryPage()),
+            ); // Changed to pop instead of pushReplacement
+          },
+          icon: Icon(Icons.arrow_back_ios_new_sharp),
+        ),
+      ),
+      body: isLoading
+          ? Center(
+        child: CircularProgressIndicator(), // Show circular progress indicator when loading
+      )
+          : GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          crossAxisSpacing: 4.0,
+          mainAxisSpacing: 4.0,
+        ),
+        itemCount: imageUrls.length,
+        itemBuilder: (context, index) {
+          return GridTile(
+            child: Image.network(imageUrls[index], fit: BoxFit.cover),
+          );
+        },
+        scrollDirection: Axis.horizontal,
+      ),
+    );
+  }
+}
