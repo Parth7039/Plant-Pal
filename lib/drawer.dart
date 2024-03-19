ac1b45d3ff2bb3ae41b04e762b59f6e4ebc0c934
Parth Bhamare <p.k.bhamare07@gmail.com>
2024-03-19 20:11:40 +0530

    hello

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:medigard/chats/users.dart';
 import 'package:medigard/my_list_tile.dart';
 import 'package:medigard/ui_pages/profile.dart';
 import 'package:medigard/ui_pages/settings.dart';
@@ -18,7 +19,7 @@ class MyDrawer extends StatelessWidget {
           )),
           MyListTile(icon:Icons.person_add, text: 'P R O F I L E', onTap: (){Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProfilePage()),);}),
           MyListTile(icon:Icons.settings, text: 'S E T T I N G S', onTap: (){Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SettingsPage()),);}),
-          MyListTile(icon:Icons.chat_outlined, text: 'C O M M U N I T Y', onTap: (){Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SettingsPage()),);}),
+          MyListTile(icon:Icons.chat_outlined, text: 'C O M M U N I T Y', onTap: (){Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ChatUsers()),);}),
         ],
       ),
     );
