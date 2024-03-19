496373f6577f289d557565546732ab3020667d28
Parth Bhamare <p.k.bhamare07@gmail.com>
2024-03-19 21:08:28 +0530

    hello

--- lib/ui_pages/profile.dart
+++ lib/ui_pages/profile.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'homepage.dart';
@@ -10,13 +11,28 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final currentUser = FirebaseAuth.instance.currentUser!;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.black,
           leading:
-          IconButton(onPressed: () { Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()) );}, icon: Icon(Icons.arrow_back_ios_new_sharp),)
+          IconButton(onPressed: () { Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()) );}, icon: Icon(Icons.arrow_back_ios_new_sharp,color: Colors.white,),)
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        onPressed: () {  },
+        child: Icon(Icons.edit,color: Colors.white,),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Icon(Icons.person,size: 130),
+            SizedBox(height: 5,),
+            Text(currentUser.email!)
+          ],
+        ),
       ),
     );
   }
